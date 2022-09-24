f834ead2216d52b3aea8e9ac2540848f1ee8827f
Sagar1555 <stimalsena155@gmail.com>
2022-09-24 16:15:06 +0545

    model and category view created

---
+++ SaagarCURD/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaagarCURD.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SaagarCURD/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SaagarCURD.Models;
+
+namespace SaagarCURD.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
